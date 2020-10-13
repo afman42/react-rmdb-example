c659984a1fdf2aaaf568dd331bdacef30a23234b
Armandos42 <afifarman50@gmail.com>
2020-10-13 19:00:38 +0700

    Rating dan Login

--- src/components/MovieInfo/index.js
+++ src/components/MovieInfo/index.js
@@ -1,15 +1,26 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import API from '../../API'
 import PropTypes from 'prop-types';
 //Components
 import Thumb from '../Thumb'
+import Rate from '../Rate'
 //Config
-import { IMAGE_BASE_URL, POSTER_SIZE } from '../../config'
+import { API_KEY, IMAGE_BASE_URL, POSTER_SIZE } from '../../config'
 //Image
 import NoImage from '../../images/no_image.jpg'
 //Styles
 import { Wrapper, Content, Text } from './MovieInfo.styles'
+// Context
+import { Context } from '../../context'
 
-const MovieInfo = ({ movie }) => (
+const MovieInfo = ({ movie }) => {
+    const [user] = useContext(Context)
+    const handleRating = async value => {
+        const rate = await API.rateMovie(user.sessionId, movie.id,value);
+        console.log(rate);
+    }
+
+    return (
     <Wrapper backdrop={movie.backdrop_path}>
         <Content>
             <Thumb
@@ -29,17 +40,24 @@ const MovieInfo = ({ movie }) => (
                         <h3>RATING</h3>
                         <div className="score">{movie.vote_average}</div>
                     </div>
+                    <div className="director">
+                        <h3>DIRECTOR {movie.directors.length > 1 ? 'S' : ''}</h3>
+                        { movie.directors.map(director => (
+                            <p key={director.credit_id}>{director.name}</p>
+                        ))}
+                    </div>
                 </div>
-                <div className="director">
-                    <h3>DIRECTOR {movie.directors.length > 1 ? 'S' : ''}</h3>
-                    { movie.directors.map(director => (
-                        <p key={director.credit_id}>{director.name}</p>
-                    ))}
-                </div>
+                {user && (
+                    <div>
+                        <p>Rate Movie</p>
+                        <Rate callback={handleRating}/>
+                    </div>
+                )}
             </Text>
         </Content>
     </Wrapper>
 )
+}
 MovieInfo.propTypes = {
     movie: PropTypes.object
 }
